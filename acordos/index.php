86599a808b3352a3277fd5de3fe953b122a2ec48
tradesishelio <99908450+tradesishelio@users.noreply.github.com>
2024-09-30 10:54:01 -0300

    Update index.php

--- acordos/index.php
+++ acordos/index.php
@@ -59,7 +59,7 @@ include_once(__DIR__ . '/../header.php');
             <!-- Coluna 2 -->
             <div class="col">
                 <div class="list-group">
-                    <a href="oferta_parametros" class="list-group-item mb-2">
+                    <a href="oferta_parametros.php" class="list-group-item mb-2">
                         <div class="row g-0">
                             <div class="col-1 text-center " style="width: 50px;">
                                 <i class="bi bi-file-earmark-text" style="font-size: 35px;"></i>
